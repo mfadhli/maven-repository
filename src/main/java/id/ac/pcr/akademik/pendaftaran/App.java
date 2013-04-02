c785375377fcc6768b401e636f703cc9d80a0a41
Mardhiah <mardhiah@pcr.ac.id>
2013-04-02 09:24:18 +0700

    commit pertama

---
+++ src/main/java/id/ac/pcr/akademik/pendaftaran/App.java
@@ -0,0 +1,14 @@
+package id.ac.pcr.akademik.pendaftaran;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("nama : mardhiah fadhli");
+    }
+}
